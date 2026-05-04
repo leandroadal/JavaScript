5c8ad2f243af71440214cb3360195d35da8f4de3
Leandro Silva <lleandro.adal@gmail.com>
2026-05-04 20:37:19 -0300

    UP: Melhorada a explicação sobre a herança

--- 41 - Objetos e Prototypes/41.06 - Herança/index.js
+++ 41 - Objetos e Prototypes/41.06 - Herança/index.js
@@ -1,4 +1,4 @@
-// Usando função
+// Herança usando função
 function Produto(nome, preco) {
     this.nome = nome;
     this.preco = preco;
@@ -13,24 +13,29 @@ Produto.prototype.aumento = function(quantia) {
     this.preco += quantia;
 };
 
+// =============== CAMISETA ===============
+
 function Camiseta(nome, preco, cor) {
-    // Para herda os atributos de Produto
-    // Não consegue herdar assim as funções no protótipo
+    // O 'Call' copia os atributos do produto, porém não de seu prototype.
     Produto.call(this, nome, preco);
     this.cor = cor;
 }
 
 // Para poder usar a função de aumento do produto a partir do objeto da camiseta
 
-// Camisa.prototype é um objeto vazio então quando recebe o objeto o construtor fica sendo o Produto
+// Camisa.prototype é um objeto vazio então quando recebe o objeto, o construtor fica sendo o Produto
 Camiseta.prototype = Object.create(Produto.prototype); 
 
 // Define a Camiseta como construtor
 Camiseta.prototype.constructor = Camiseta;
 
+// Sobrescrevendo o método do aumento que estava no Produto
 Camiseta.prototype.aumento = function(percentual) {
     this.preco = this.preco + (this.preco * (percentual / 100));
 };
+// ==========================================
+
+// =============== CANECA ===============
 
 function Caneca(nome, preco, material, estoque) {
     Produto.call(this, nome, preco);
@@ -52,14 +57,20 @@ function Caneca(nome, preco, material, estoque) {
 Caneca.prototype = Object.create(Produto.prototype);
 Caneca.prototype.constructor = Caneca;
 
+// ================================================
+
 const produto = new Produto('Garrafa', 545);
 const camiseta = new Camiseta('Regata', 3.8, 'Verde');
 const caneca = new Caneca('Caneca', 12, 'Plástico', 5);
 
 produto.aumento(10);
 camiseta.aumento(10);
-caneca.estoque = '520';
+caneca.estoque = '520'; // Ignora a string por causa do set do estoque.
 
 console.log(produto);
 console.log(camiseta);
 console.log(caneca);
+console.log('Estoque caneca:', caneca.estoque);
+
+camiseta.preco = '1000'; // Preço vira uma string 
+console.log('Camiseta com o preço modificado:', camiseta);
