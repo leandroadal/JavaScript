44a9776903e4ed03f86843e520b3f744b401850c
Leandro Silva <lleandro.adal@gmail.com>
2026-05-09 11:28:54 -0300

    ADD: Express

---
+++ 45 - Node, Npm, Express e MongoDB/45.06 - Express - Introducao/REAME.md
@@ -0,0 +1,17 @@
+# Express
+
+O Express tem o objetivo de simplificar a criação de servidores HTTP, rotas, middlewares e o tratamento de requisições e respostas. O framework abstrai funcionalidades do módulo HTTP nativo do Node.js, reduzindo a complexidade do código e tornando o desenvolvimento mais produtivo.
+
+## Principais funcionalidades
+
+O Express permite:
+
+* criação de servidores web;
+definição de rotas;
+* tratamento de requisições HTTP;
+* utilização de middlewares;
+* criação de APIs REST;
+
+## Instalação
+
+`npm i express`
---
+++ 45 - Node, Npm, Express e MongoDB/45.06 - Express - Introducao/server.js
@@ -0,0 +1,32 @@
+/**
+ * GET -> Ler
+ * POST -> Criar 
+ * PUT -> Atualizar
+ * DELETE -> Apagar
+ */
+
+const express = require('express');
+const app = express();
+
+// Para requisições GET
+app.get('/', (req, res) => {
+    res.send(`
+        Hello world!
+
+        <form action="/" method="POST">
+        Nome: <input type="text" name="nome">
+        <button>Enviar</button>
+        </form>
+        `);
+});
+
+// Para requisições POST
+app.post('/', (req, res) => {
+    res.send(`Recebi o formulário`);
+});
+
+// Para o servidor ficar escutando na porta 3000
+app.listen(3000, () => {
+    console.log('Acessar http://localhost:3000');
+    console.log('Servidor executando na porta 3000');
+});
